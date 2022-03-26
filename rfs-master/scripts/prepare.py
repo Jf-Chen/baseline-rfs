8f1c9d712bf431ca71817609af83ae7fc1bda38f
Jf-Chen <485133380@qq.com>
2022-03-26 21:53:25 +0800

    update
    注意从dropbox下载文件时的正确做法

--- rfs-master/scripts/prepare.py
+++ rfs-master/scripts/prepare.py
@@ -6,7 +6,7 @@ import argparse
 parser = argparse.ArgumentParser(description="prepare datasets", formatter_class=argparse.RawTextHelpFormatter)
 
 # parser.add_argument("--dataset_name", type=str, default="miniImageNet", help="dataset name")
-parser.add_argument("--tar_from_path", type=str, default="/content/drive/MyDrive/easy/easy-datasets.tar", help="提取路径")
+parser.add_argument("--tar_from_path", type=str, default="/content/drive/MyDrive/rfs/miniImageNet.tar.gz", help="提取路径")
 parser.add_argument("--tar_to_floder", type=str, default="", help="解压位置")
 parser.add_argument("--tar_type", type=str, default="tar", help="解压位置")
 
