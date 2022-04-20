63bf40bee6fd8e6090c9458710c713cc70a651fa
Jf-Chen <485133380@qq.com>
2022-04-20 09:48:05 +0800

    Update prepare.sh

--- rfs-master/scripts/prepare.sh
+++ rfs-master/scripts/prepare.sh
@@ -1,9 +1,9 @@
 pip3 install tensorboardX
-echo "tensorboardX installed"
-echo "current path is:"
+echo "----tensorboardX installed"
+echo "----current path is:"
 echo $PWD
 
 chmod +777 /content/baseline-rfs/rfs-master/scripts/prepare.py 
 python /content/baseline-rfs/rfs-master/scripts/prepare.py  --tar_from_path /content/drive/MyDrive/rfs/miniImageNet.tar.gz  --tar_to_floder ./data_root  --tar_type tar --use_teacher
 
-echo "dataset ready"
+echo "----dataset ready"
