629530535f90082bf7fda89cdecfafc6ea8bbb1a
Jf-Chen <485133380@qq.com>
2022-04-20 10:55:16 +0800

    update

--- rfs-master/scripts/prepare.py
+++ rfs-master/scripts/prepare.py
@@ -22,16 +22,16 @@ except :
 
 # 如果使用教师网络
 if args.use_teacher:
-    print("download pre-train teacher")
+    print("---- download pre-train teacher")
     shutil.unpack_archive(args.teacher_from ,"./",format=args.teacher_type) #默认解压到当前目录
     # 解压出来是/content/baseline-rfs/rfs-master/model_path/resnet12_miniImageNet_lr_0.05_decay_0.0005_trans_A_trial_pretrain，太长了
-    os.rename("./model_path/resnet12_miniImageNet_lr_0.05_decay_0.0005_trans_A_trial_pretrain","./model_path/teacher")
+    # os.rename("./model_path/resnet12_miniImageNet_lr_0.05_decay_0.0005_trans_A_trial_pretrain","./model_path/teacher")
     
     
 # 下载数据集
 
 if not os.path.exists(args.tar_to_floder):
-    print("args.tar_to_floder not exist,now create")
+    print("--- args.tar_to_floder not exist,now create")
     os.makedirs(args.tar_to_floder) 
 
 shutil.unpack_archive(args.tar_from_path, args.tar_to_floder,args.tar_type)
@@ -39,17 +39,17 @@ shutil.unpack_archive(args.tar_from_path, args.tar_to_floder,args.tar_type)
 # 创建其他文件夹
 model_path="./model_path"
 if not os.path.exists(model_path):
-    print("./model_path not exist,now create")
+    print("---- ./model_path not exist,now create")
     os.makedirs(model_path) 
 
 tensorboard_logger="./tb_path"
-if not os.path.exists(model_path):
-    print("./tb_path not exist,now create")
+if not os.path.exists(tensorboard_logger):
+    print("----./tb_path not exist,now create")
     os.makedirs(tensorboard_logger) 
     
 data_root="./data_root"
-if not os.path.exists(model_path):
-    print("./data_root not exist,now create")
+if not os.path.exists(data_root):
+    print("---- ./data_root not exist,now create")
     os.makedirs(data_root) 
     
 
--- rfs-master/scripts/prepare.sh
+++ rfs-master/scripts/prepare.sh
@@ -7,3 +7,6 @@ chmod +777 /content/baseline-rfs/rfs-master/scripts/prepare.py
 python /content/baseline-rfs/rfs-master/scripts/prepare.py  --tar_from_path /content/drive/MyDrive/rfs/miniImageNet.tar.gz  --tar_to_floder ./data_root  --tar_type tar --use_teacher
 
 echo "----dataset ready"
+
+# ======================
+python train_distillation.py -r 0.5 -a 0.5 --path_t ./model_path/resnet12_miniImageNet_lr_0.05_decay_0.0005_trans_A_trial_pretrain/ckpt_epoch_100.pth --trial born1 --model_path ./model_path --tb_path ./tb_path --data_root ./data_root
