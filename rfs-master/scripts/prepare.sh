b11167a4329d4c4963c44c05655b4505ec6ab768
Jf-Chen <485133380@qq.com>
2022-04-23 10:12:18 +0800

    Update prepare.sh

--- rfs-master/scripts/prepare.sh
+++ rfs-master/scripts/prepare.sh
@@ -9,6 +9,6 @@ python /content/baseline-rfs/rfs-master/scripts/prepare.py  --tar_from_path /con
 echo "----dataset ready"
 
 # ======================
-python train_distillation.py -r 0.5 -a 0.5 --path_t ./model_path/resnet12_miniImageNet_lr_0.05_decay_0.0005_trans_A_trial_pretrain/ckpt_epoch_100.pth --trial born1 --model_path ./model_path --tb_path ./tb_path --data_root ./data_root
+#python train_distillation.py -r 0.5 -a 0.5 --path_t ./model_path/resnet12_miniImageNet_lr_0.05_decay_0.0005_trans_A_trial_pretrain/ckpt_epoch_100.pth --trial born1 --model_path ./model_path --tb_path ./tb_path --data_root ./data_root
 
-echo "----distill accomplished"
+#echo "----distill accomplished"
